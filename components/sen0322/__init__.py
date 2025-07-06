1c05cc5d7080a5bd2b39e8045ad2616908a01d13
makbart1980 <makbart666@gmail.com>
2025-07-06 13:15:20 +0200

    Create __init__.py

---
+++ components/sen0322/__init__.py
@@ -0,0 +1,7 @@
+import esphome.codegen as cg
+
+# Define the namespace for this component
+sen0322_ns = cg.esphome_ns.namespace("sen0322")
+
+# This file is required for ESPHome to recognize this directory as a component package
+# The namespace is used by sensor.py to reference C++ classes
